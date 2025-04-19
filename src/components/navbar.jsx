7df0e038d0aba5b6bc5cf539389b983e3eb15495
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2025-04-20 04:37:38 +0530

    updated landing page

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
   function Navbar() {
+    const [isHover, setisHover] = useState(false);
     return (
-      <div className="h-26 w-full bg-white/0 flex items-center font-[Poppins] justify-around z-[100] fixed top-0 left-0 select-none">
+      <div onMouseEnter = {()=>setisHover(true)} onMouseLeave = {()=>setisHover(false)} className="h-26 w-full bg-white/0 flex items-center font-[Poppins] justify-around z-[100] fixed top-0 left-0 select-none">
+        {isHover ? (
         <div className="font-extrabold text-2xl transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ">
           <span>
-            f<span className="text-red-600">o</span>c
+            f<span className="text-red-600 ">o</span>c
             <span className="text-red-600">o</span>
           </span>
         </div>
+        ) : (
+          <div className="font-extrabold text-2xl transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-red-600 ">
+          <span>
+            f<span className="text-black ">o</span>c
+            <span className="text-black">o</span>
+          </span>
+        </div>
+        ) }
 
         <div className="max-w-[40%] flex text-[15px] gap-16 justify-start">
 
-          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">Home</div>
-          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 ">Menu</div>
-          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 ">Services</div>
-          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 ">Contact</div>
+          <div className="cursor-pointer  transition delay-0 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:border-b-2">Home</div>
+          <div className="cursor-pointer  transition delay-0 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:border-b-2">Menu</div>
+          <div className="cursor-pointer  transition delay-0 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:border-b-2">Services</div>
+          <div className="cursor-pointer  transition delay-0 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:border-b-2">Contact</div>
         </div>
 
     
         <div className="flex gap-4">
-          <button className="text-black px-6 py-2  bg-[#ffcc70] border-none rounded-full cursor-pointer font-medium overflow-hidden">
+          <button className="text-black px-6 py-2  bg-[#f2d49c] border-none rounded-full cursor-pointer font-medium overflow-hidden hover:bg-amber-400 hover:scale-105">
             Sign in
           </button>
-          <button className="px-6 py-2 bg-[#ffcc70] border-none rounded-full cursor-pointer font-medium">
+          <button className="px-6 py-2 bg-[#f2d49c] border-none rounded-full cursor-pointer font-medium hover:bg-amber-400 transtion delay-75 duration-200 ease-out hover:scale-105">
             Login
           </button>
         </div>
