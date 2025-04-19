ed647a32dfc502d4ac13f5cc96e1bdd2d40d460a
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2025-04-19 23:44:53 +0530

    navbar fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,84 +1,34 @@
 import React from 'react';
 
-function Navbar() {
-  return (
-    <div
-      style={{
-        height: "7rem",
-        width: "100%",
-        backgroundColor: "rgba(255, 255, 255, 0.0)",
-        display: "flex",
-        alignItems: "center",
-        padding: "0 2rem",
-        fontFamily: "Poppins",
-        justifyContent: "space-between",
-        zIndex: "1000",
-        position: "fixed",
-        top: "0",
-        left: "0",
-      }}
-    >
-      <div
-        style={{
-          fontWeight: "bolder",
-          fontSize: "1.5rem",
-          padding: "4rem",
-        }}
-      >
-        <span>
-          f<span style={{ color: "red" }}>o</span>c
-          <span style={{ color: "red" }}>o</span>
-        </span>
-      </div>
+  function Navbar() {
+    return (
+      <div className="h-26 w-full bg-white/0 flex items-center font-[Poppins] justify-around z-[100] fixed top-0 left-0 select-none">
+        <div className="font-extrabold text-2xl transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 ">
+          <span>
+            f<span className="text-red-600">o</span>c
+            <span className="text-red-600">o</span>
+          </span>
+        </div>
 
-      <div
-        style={{
-          display: "flex",
-          gap: "4rem",
-          fontSize: "15px",
-          marginRight: "32rem",
-        }}
-      >
-        <div style={{ cursor: "pointer" }}>Home</div>
-        <div style={{ cursor: "pointer" }}>Menu</div>
-        <div style={{ cursor: "pointer" }}>Services</div>
-        <div style={{ cursor: "pointer" }}>Contact</div>
-      </div>
+        <div className="max-w-[40%] flex text-[15px] gap-16 justify-start">
 
-      <div
-        style={{
-          display: "flex",
-          gap: "1rem",
-        }}
-      >
-        <button
-          style={{
-            color : "white",
-            padding: "0.8rem 2rem",
-            backgroundColor: "transparent",
-            border: "1px solid white",
-            borderRadius: "20px",
-            cursor: "pointer",
-            fontWeight: "500",
-          }}
-        >
-          Sign in
-        </button>
-        <button
-          style={{
-            
-            padding: "0.8rem 2rem",
-            backgroundColor: "#ffcc70",
-            border: "none",
-            borderRadius: "20px",
-            cursor: "pointer",
-            fontWeight: "500",
-          }}
-        >
-          Login
-        </button>
+          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">Home</div>
+          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 ">Menu</div>
+          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 ">Services</div>
+          <div className="cursor-pointer  transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 ">Contact</div>
+        </div>
+
+    
+        <div className="flex gap-4">
+          <button className="text-black px-6 py-2  bg-[#ffcc70] border-none rounded-full cursor-pointer font-medium overflow-hidden">
+            Sign in
+          </button>
+          <button className="px-6 py-2 bg-[#ffcc70] border-none rounded-full cursor-pointer font-medium">
+            Login
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
-export default Navbar;
+    );
+  }
+
+  export default Navbar;
