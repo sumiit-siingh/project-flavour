426998e28a3f0545422501244d1c10f7332429a3
Sumit Singh <sumitsinghsakarari@gmail.com>
2025-04-20 05:32:16 +0530

    fix

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { motion } from "motion/react"
+
 function Menu(){
     return (
+    <>
         <div className="h-[150vh]  bg-red-200">
           <div className="bg-white mt-10">Hey, What's you Food mood?</div>
 
 
         </div>
         <div className="bg-white sm:ml-5 md:ml-7 xl:ml-10 mt-15 font-semibold text-xl">Top discounts for you</div>
-      </div>
+      </>
       
     )
 }
